5d13a12965a771d356704492c6abb9ff1a974e2c
Fartunn <fartunabdulkadir65@gmail.com>
2024-08-12 17:16:27 -0500

    Initial commit of assignment files

---
+++ app/build.gradle.kts
@@ -0,0 +1,96 @@
+import org.jetbrains.kotlin.psi.simpleNameExpressionVisitor
+
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+    id("jvm-test-suite")
+}
+
+android {
+    namespace = "edu.metrostate.testingdemo"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "edu.metrostate.testingdemo"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+    testLogging {
+        events("passed", "skipped", "failed")
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.lifecycle.runtime.compose)
+    implementation(libs.kotlinx.coroutines.android)
+    implementation(libs.retrofit)
+
+    // These are the test dependencies you will need to test the view models and coroutines
+    // After updating your libs.versions.toml, update your build.gradle with these dependencies.
+    testImplementation(libs.junit)
+    testImplementation(libs.mockk.android)
+    testImplementation(libs.mockk.agent.android)
+    testImplementation(libs.kotlinx.coroutines.test)
+    testImplementation(libs.androidx.arch.core)
+    testRuntimeOnly(libs.junit.engine)
+    // end test dependencies
+
+
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+}
---
+++ app/src/test/java/edu/metrostate/testingdemo/MainViewModelTest.kt
@@ -0,0 +1,116 @@
+package edu.metrostate.testingdemo
+
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.every
+import io.mockk.mockk
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNull
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+
+/**
+ * Please note that you will need both of these extensions if you are testing coroutines and
+ * view models with LiveData. They can be found in the testing package.
+ *
+ * Also, review the libs.versions.toml and the app build.gradle.kts files to make sure you have
+ * the proper test dependencies set up in your project.
+ */
+@ExtendWith(CoroutinesTestExtension::class, InstantExecutorExtension::class)
+class MainViewModelTest {
+
+    @Test
+    fun `login() should be successful with correct credentials`() = runTest {
+        /**
+         * Create the mock User and define the mock responses
+         */
+        val user = mockk<User>()
+        every { user.name } returns "User Name"
+        every { user.id } returns "ID"
+        every { user.email } returns "user@name.com"
+
+        /**
+         * Create the mock ApiService and mock the login() response.
+         */
+        val api = mockk<ApiService>()
+        coEvery { api.login(any(), any()) } returns user
+
+        /**
+         * Create the system under test. In this case MainViewModel.
+         */
+        val viewModel = MainViewModel(api)
+
+        /**
+         * Collect the emitted view states for later verification
+         */
+        val viewStates = mutableListOf<MainViewModel.ViewState>()
+        viewModel.viewState.observeForever {
+            viewStates.add(it)
+        }
+
+        viewModel.login("username", "password1")
+
+        /**
+         * Assert that the returned values from the view model User object are correct.
+         */
+        assertEquals("User Name", viewModel.userState.value?.name)
+        assertEquals("ID", viewModel.userState.value?.id)
+        assertEquals("user@name.com", viewModel.userState.value?.email)
+
+        /**
+         * Assert that the view model emitted the view states in the correct order.
+         */
+        assertEquals(
+            listOf(MainViewModel.ViewState.Loading, MainViewModel.ViewState.Success),
+            viewStates
+        )
+
+        /**
+         * Verify that the api service login function was called with the correct arguments.
+         */
+        coVerify { api.login("username", "password1") }
+    }
+
+    @Test
+    fun `login() should fail with incorrect credentials`() = runTest {
+        /**
+         * Mock ApiService to throw an exception when login is called with incorrect credentials
+         */
+        val api = mockk<ApiService>()
+        coEvery { api.login(any(), any()) } throws RuntimeException("")
+
+        /**
+         * Create the system under test. In this case MainViewModel.
+         */
+        val viewModel = MainViewModel(api)
+
+        /**
+         * Collect the emitted view states for later verification
+         */
+        val viewStates = mutableListOf<MainViewModel.ViewState>()
+        viewModel.viewState.observeForever {
+            viewStates.add(it)
+        }
+
+        viewModel.login("wrongUsername", "wrongPassword")
+
+        /**
+         * Assert that the User object in the view model remains null after a failed login attempt
+         */
+        assertNull(viewModel.userState.value)
+
+        /**
+         * Assert that the view model emitted the view states in the correct order.
+         */
+        assertEquals(
+            listOf(MainViewModel.ViewState.Loading, MainViewModel.ViewState.Error("Failed")),
+            viewStates
+        )
+
+        /**
+         * Verify that the api service login function was called with the correct arguments.
+         */
+        coVerify { api.login("wrongUsername", "wrongPassword") }
+    }
+}
